6c4afeeb66d20a897750ddd36f745c55b66279ab
farmerbrian <nichols.brian.d@gmail.com>
2022-04-12 13:57:44 -0500

    Add Score keeping variables

--- script.js
+++ script.js
@@ -6,32 +6,32 @@ function computerPlay() {
     return choice;
 }
 
-/*let computerSelection = computerPlay();
 
- Is it randomly choosing properly for the computer? 
-console.log(computerSelection);
-*/
-/* Prompt the player to enter a selection and store in a variable called playerSelection 
+let playerScore = 0;
+let computerScore = 0;
 
-let playerSelection = prompt('Choose Wisely', '');
-*/
 
-/*
-console.log(playerSelection);
- Create a function that takes playerSelection and computerSelection and plays a round of the game */
+/* Create a function that takes playerSelection and computerSelection and plays a round of the game */
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == 'rock' && computerSelection == 'scissors') {
         result = (`You Win, ${playerSelection} beats ${computerSelection}!!!`);
+        playerScore++;
     } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
         result = (`You Win, ${playerSelection} beat ${computerSelection}!!!`);
+        playerScore++;
     } else if (playerSelection == 'paper' && computerSelection == 'rock') {
         result = (`You Win, ${playerSelection} beats ${computerSelection}!!!`);
+        playerScore++;
     } else if (playerSelection == computerSelection) {
         result = (`You Tied!!! ${computerSelection} equals ${playerSelection}`);
+        playerScore++;
+        computerScore++;
     } else if (playerSelection != 'rock' || 'paper' || 'scissors') {
         result = (`You loose ${playerSelection} is not a valid guess`);
+        computerScore++;
     } else {result = (`You loose, ${computerSelection} beats ${playerSelection}!!!`);
+    computerScore++;
     }
     return result;
 }
@@ -46,8 +46,11 @@ function game(computerPlay,playRound) {
 
     for (let i = 0; i < 5; i++) {
         let computerSelection = computerPlay();
+/* Is it actually radomly picking a valid option? */
         console.log(computerSelection);
+/* Prompt the player to enter a selection and store in a variable called playerSelection */
         let playerSelection = prompt('Choose Wisely', '').toLowerCase();
+/* Is it assigning to the variable as all lowercase? */
         console.log(playerSelection);
         let roundEndMessage = playRound(playerSelection, computerSelection);
         console.log(roundEndMessage);  
