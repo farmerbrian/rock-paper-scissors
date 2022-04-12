83bebf96dee053a23c035412af1b0385d6a1ac5e
farmerbrian <nichols.brian.d@gmail.com>
2022-04-12 12:21:01 -0500

    Add playRound Function and make 1 round playable

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ let computerSelection = computerPlay();
 /* Is it randomly choosing properly for the computer? */
 console.log(computerSelection);
 
-
 /* Prompt the player to enter a selection and store in a variable called playerSelection */
 
 let playerSelection = prompt('Choose Wisely', '');
@@ -19,6 +18,19 @@ let playerSelection = prompt('Choose Wisely', '');
 console.log(playerSelection);
 /* Create a function that takes playerSelection and computerSelection and plays a round of the game */
 
-/*
-function playRound()
-*/
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        result = (`You Win, ${playerSelection} beats ${computerSelection}!!!`);
+    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        result = (`You Win, ${playerSelection} beat ${computerSelection}!!!`);
+    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        result = (`You Win, ${playerSelection} beats ${computerSelection}!!!`);
+    } else if (playerSelection == computerSelection) {
+        result = (`You Tied!!!`);
+    } else {result = (`You loose, ${computerSelection} beats ${playerSelection}!!!`);
+    }
+    return result;
+}
+
+let roundEndMessage = playRound(playerSelection.toLowerCase, computerSelection.toLowerCase);
+console.log(roundEndMessage);
