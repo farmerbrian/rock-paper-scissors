4086a36c1e3bf007e6725df0f31a8a577c84b154
farmerbrian <nichols.brian.d@gmail.com>
2022-04-12 08:23:27 -0500

    Add comments to  script.js to outline the program

---
+++ script.js
@@ -0,0 +1,17 @@
+/* Start the game by randomly selecting the computers choice and storing in variable called computerSelection*/
+
+function computerPlay()
+
+
+
+
+/* Is it randomly choosing properly for the computer? */
+console.log(computerSelection);
+
+
+/* Prompt the player to enter a selection and store in a variable called playerSelection */
+
+
+/* Create a function that takes playerSelection and computerSelection and plays a round of the game */
+
+function playRound()
